57323c4b9bc9a9d1d205ef074d4942a5477ed405
Muneeb Ahmed <muneebahmed0305@gmail.com>
2023-10-23 18:03:31 +0500

    Commit

--- TMS-FrontEnd/src/App.jsx
+++ TMS-FrontEnd/src/App.jsx
@@ -1,30 +1,11 @@
 import Authentication from './Auth/Authentication';
-import { AuthProvider } from './context/AuthProvider';
-import { CommentProvider } from './context/CommentProvider';
-import { ProjectProvider } from './context/ProjectProvider';
-import { TaskProvider } from './context/TaskProvider';
-import { TeamProvider } from './context/TeamProvider';
-import { UserProvider } from './context/UserProvider';
-
+import AppProviders from './context/AppProviders';
 function App() {
 
   return (
-    <AuthProvider>
-      <ProjectProvider>
-        <UserProvider>
-          <TeamProvider>
-            <TaskProvider>
-              <CommentProvider>
-                
-                {/* Start-Up Page */}
-                <Authentication />   
-
-              </CommentProvider>
-            </TaskProvider>
-          </TeamProvider>
-        </UserProvider>
-      </ProjectProvider>
-    </AuthProvider>
+    <AppProviders>
+      <Authentication />
+    </AppProviders>
   );
 }
 
---
+++ TMS-FrontEnd/src/context/AppProviders.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { AuthProvider } from './AuthProvider'
+import { ProjectProvider } from './ProjectProvider'
+import { UserProvider } from './UserProvider'
+import { TeamProvider } from './TeamProvider'
+import { TaskProvider } from './TaskProvider'
+import { CommentProvider } from './CommentProvider'
+
+export default function AppProviders({ children }) {
+    return (
+        <AuthProvider>
+            <ProjectProvider>
+                <UserProvider>
+                    <TeamProvider>
+                        <TaskProvider>
+                            <CommentProvider>
+
+                                {children}
+
+                            </CommentProvider>
+                        </TaskProvider>
+                    </TeamProvider>
+                </UserProvider>
+            </ProjectProvider>
+        </AuthProvider>
+    )
+}
