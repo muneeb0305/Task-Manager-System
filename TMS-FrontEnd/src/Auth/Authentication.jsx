80472f5d1294af7dc1731e433f907f8ec18857fa
Muneeb Ahmed <muneebahmed0305@gmail.com>
2023-10-25 17:13:47 +0500

    Completed

--- TMS-FrontEnd/src/Auth/Authentication.jsx
+++ TMS-FrontEnd/src/Auth/Authentication.jsx
@@ -14,6 +14,7 @@ export default function Authentication() {
         }
         else {
             clearToken()
+            navigate('/')
         }
         // eslint-disable-next-line
     }, [])
