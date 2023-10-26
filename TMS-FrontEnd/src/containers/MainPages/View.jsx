32cf6dcb8b6571f5ce26543662a33e97d37fe8d1
Muneeb Ahmed <muneebahmed0305@gmail.com>
2023-10-26 17:23:12 +0500

    Completed

--- TMS-FrontEnd/src/containers/MainPages/View.jsx
+++ TMS-FrontEnd/src/containers/MainPages/View.jsx
@@ -4,34 +4,13 @@ import { useUserData } from '../../context/UserProvider'
 import { useProjectData } from '../../context/ProjectProvider'
 import { useAuth } from '../../context/AuthProvider'
 import { useTeamData } from '../../context/TeamProvider'
-import Alert from '../../components/Alert'
 
 export default function View({ display }) {
     // Get Data from Provider
-    const { user, getUser, removeUser } = useUserData()
-    const { project, getProject, removeProject } = useProjectData()
-    const { team, getTeam, removeTeam, getUserTeam } = useTeamData()
+    const { user, removeUser } = useUserData()
+    const { project, removeProject } = useProjectData()
+    const { team, removeTeam } = useTeamData()
     const { userDetail } = useAuth()
-
-    //Retrive all Data
-    useEffect(() => {
-        if (display === 'user') {
-            getUser()
-                .catch((err) => Alert({ icon: 'error', title: err }))
-        }
-        else if (display === 'project') {
-            getProject()
-                .catch((err) => Alert({ icon: 'error', title: err }))
-        }
-        else if (display === 'team') {
-            userDetail.role === 'admin' ?
-                getTeam()
-                    .catch((err) => { Alert({ icon: 'error', title: err }) }) :
-                getUserTeam()
-                    .catch((err) => { Alert({ icon: 'error', title: err }) })
-        }
-        // eslint-disable-next-line
-    }, [display])
 
     // Table Configurations of User, Team, Project and Task
     const tableConfigs = {
